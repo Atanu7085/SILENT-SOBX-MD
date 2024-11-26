6600c667dc68d573d3612b98733703c7ca8da5b5
Atanu7085 <atanu468@outlook.com>
2024-11-26 12:12:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HuZ2mTga#t2uqCZG0Gb7vwglAU4xDVgwb8DQXmmrJt2ThVTxhMgU",
+SESSION_ID: process.env.SESSION_ID || "fm51UITS#OJEbol_JzdBRFHN8b-qzlFmnZMhrhqpt_ZXdzJLRN6k",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
@@ -14,10 +14,10 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SILENT-SOBX-MD WHATSAPP BOT 😊♻️",
 ANTI_LINK: process.env.ANTI_LINK || "false",
-ANTI_BAD: process.env.ANTI_BAD || "true",
+ANTI_BAD: process.env.ANTI_BAD || "false",
 PREFIX: process.env.PREFIX || ".",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
-AUTO_REACT: process.env.AUTO_REACT || "true",
-OWNER_REACT: process.env.OWNER_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+OWNER_REACT: process.env.OWNER_REACT || "False ",
 BOT_NAME: process.env.BOT_NAME || "ATANU_BOT-OFF",
 };
